7ab8d31d2834efd521909996fa65a67403f1dcd6
yen-mai-codes <yenngocmai.work@gmail.com>
2023-06-08 21:58:55 +0700

    chore: add base bonus into git repo

--- bonus/stage_1.py
+++ bonus/stage_1.py
@@ -3,6 +3,7 @@ Stage 1 of our fictional pipeline
 """
 
 from subprocess import Popen, PIPE, STDOUT
+file_path = '/media/yenhocdungubuntu/Main File Box/Jobs_Internship/Blueoc/Wk03/python-basics-2/bonus/mysterious_generation_script.sh'
 
 
 def stage_execute(num_files: int, date: str) -> str:
@@ -10,6 +11,6 @@ def stage_execute(num_files: int, date: str) -> str:
     Executes the stage 1.
     """
     print("Executing stage 1..")
-    output = Popen(["./mysterious_generation_script.sh", str(num_files), date], stdout=PIPE, stderr=STDOUT)
+    output = Popen([file_path, str(num_files), date], stdout=PIPE, stderr=STDOUT)
     stdout, _ = output.communicate()
     return stdout.decode("utf-8")
