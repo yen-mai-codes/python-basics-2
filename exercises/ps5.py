854ce133fd2c005de3dcc485fafb82db1b5389b6
yen-mai-codes <yenngocmai.work@gmail.com>
2023-06-08 18:03:58 +0700

    feat: user-specified triggers passed tests

--- exercises/ps5.py
+++ exercises/ps5.py
@@ -25,6 +25,7 @@ import re
 
 
 def process(url):
+    found = False
     """
     Fetches news items from the rss url and parses them.
     Returns a list of NewsStory-s.
@@ -48,7 +49,8 @@ def process(url):
         #  pubdate = pubdate.astimezone(pytz.timezone('EST'))
         #  pubdate.replace(tzinfo=None)
         except ValueError:
-            pubdate = datetime.strptime(pubdate, "%a, %d %b %Y %H:%M:%S %z")
+            pubdate = datetime.strptime(pubdate, "%Y-%m-%dT%H:%M:%SZ")
+            pubdate.replace(tzinfo=pytz.timezone("GMT"))
 
         newsStory = NewsStory(guid, title, description, link, pubdate)
         ret.append(newsStory)
@@ -293,11 +295,15 @@ def filter_stories(stories, triggerlist):
     """
     filtered_stories = []
 
+    # loop through each story, then for each story, loop through list of triggers
+    # if there's any trigger that satisfies current story, add story to filtered stories
+    # break loop when there's a satisfactory trigger
     for story in stories:
         for trigger in triggerlist:
             if trigger.evaluate(story):
                 filtered_stories.append(story)
-    
+
+                break
     return filtered_stories
 
 
@@ -325,9 +331,132 @@ def read_trigger_config(filename):
     # line is the list of lines that you need to parse and for which you need
     # to build triggers
 
-    print(lines)  # for now, print it so you see what it contains!
+    triggers_dict = {}
+    final_triggers = []
 
+    for line in lines:
+        # split line into elements
+        line = line.split(',')
 
+        # trigger definition: Lines that do not begin with the keyword ADD define named triggers.
+        if line[0] != 'ADD':
+            # if there are just 2 args, then it's a named trigger with the other argument as a negated trigger
+            if len(line) == 2:
+                trig_name = line[0]
+                negated_other_trig = line[1].split()
+                trig_type = negated_other_trig[0]
+                other_trig_name = negated_other_trig[1]
+                
+                if trig_type == 'NOT':
+                    # makes sure the other trig has been declared
+                    try: 
+                        other_trig = triggers_dict[other_trig_name]
+                        triggers_dict[trig_name] = NotTrigger(other_trig)
+                    except KeyError:
+                        raise KeyError(other_trig_name, 'has not been declared as a trigger yet!')
+                    
+            # if there are 3 arguments, then it's to name a simple trigger
+            # simple trigs:
+            # - title
+            # - description
+            # - after 
+            # - before
+            if len(line) == 3:
+                trig_name = line[0]
+                trig_type = line[1]
+                trig_arg = line[2]
+                if trig_type == 'TITLE':
+                    triggers_dict[trig_name] = TitleTrigger(trig_arg)
+                elif trig_type == 'DESCRIPTION':
+                    triggers_dict[trig_name] = DescriptionTrigger(trig_arg)
+                elif trig_type == 'BEFORE':
+                    triggers_dict[trig_name] = BeforeTrigger(trig_arg)
+                elif trig_type == 'AFTER':
+                    triggers_dict[trig_name] = AfterTrigger(trig_arg)
+
+            # if there are 4 arguments, then it's a composite trigger
+            # composite trigs:
+            # - AND
+            # - OR
+            # note: a composite trig can consist of negated (NOT) triggers!
+            elif len(line) == 4:
+                trig_name = line[0]
+                trig_type = line[1]
+                arg1 = line[2].split()
+                arg2 = line[3].split()
+
+                arg1_negated = False
+                arg2_negated = False
+                arg1_len = len(arg1)
+                arg2_len = len(arg2)
+                
+                # check if arg1 is a negated trig
+                if arg1_len == 1:
+                    arg1 = arg1[0]
+                elif arg1_len == 2 and arg1[0] == 'NOT':
+                    arg1_negated = True
+                    arg1 = arg1[1]
+
+                if arg2_len == 1:
+                    arg2 = arg2[0]
+
+                elif arg2_len == 2 and arg2[0] == 'NOT':
+                    arg2_negated = True
+                    arg2 = arg2[1]
+
+                # make sure that both args have been declared
+                try:
+                    arg1 = triggers_dict[arg1]
+                except:
+                    raise KeyError(arg1, 'has not been declared as a trigger yet!')
+                try: 
+                    arg2 = triggers_dict[arg2]
+                except:
+                    raise KeyError(arg2, 'has not been declared as a trigger yet!')
+                
+                # if any of the triggers are negated, declare negation using NotTrigger
+                if arg1_negated:
+                    arg1 = NotTrigger(arg1)
+                if arg2_negated:
+                    arg2 = NotTrigger(arg2)
+                
+                # check either type is AND or OR
+                if trig_type == 'AND':
+                    triggers_dict[trig_name] = AndTrigger(arg1, arg2)
+
+                elif trig_type == 'OR':
+                    triggers_dict[trig_name] = OrTrigger(arg1, arg2)
+        
+        # add triggers
+        # note: can consist of negated (NOT) triggers!
+        else:
+
+            #go thru the list of triggers to add
+            for i in range (1, len(line)):
+                trig = line[i].split()
+                trig_len = len(trig)
+                trig_negated = False
+
+                # check if trig is a negated trig
+                if trig_len == 1:
+                    trig = trig[0]
+                elif trig_len == 2 and trig[0] == 'NOT':
+                    trig_negated = True
+                    trig = trig[1]
+
+                # check if trig has been declared
+                try:
+                    trig = triggers_dict[trig]
+                    # if trig is negated, declare its negation using NotTrigger
+                    if trig_negated:
+                        trig = NotTrigger(trig)
+                    # add trigger to final triggers list
+                    final_triggers.append(trig)
+
+                except:
+                    raise KeyError(trig_name, 'has not been declared as a trigger!')
+
+    return final_triggers
 SLEEPTIME = 120  # seconds -- how often we poll
 
 
@@ -335,7 +464,9 @@ def main_thread(master):
     # A sample trigger list - you might need to change the phrases to correspond
     # to what is currently in the news
     # try:
-    t1 = TitleTrigger("election")
+    t0 = TitleTrigger('NASA')
+    t00 = TitleTrigger("Trump")
+    t1 = OrTrigger(t0, t00)
     t2 = DescriptionTrigger("Trump")
     t3 = DescriptionTrigger("Clinton")
     t4 = AndTrigger(t2, t3)
@@ -343,7 +474,7 @@ def main_thread(master):
 
     # Problem 11
     # TODO: After implementing read_trigger_config, uncomment this line
-    # triggerlist = read_trigger_config('triggers.txt')
+    triggerlist = read_trigger_config('triggers.txt')
 
     # HELPER CODE - you don't need to understand this!
     # Draws the popup window that displays the filtered stories
@@ -379,7 +510,7 @@ def main_thread(master):
         stories = process("http://news.google.com/news?output=rss")
 
         # Get stories from Yahoo's Top Stories RSS news feed
-        stories.extend(process("http://news.yahoo.com/rss/topstories"))
+        # stories.extend(process("http://news.yahoo.com/rss/topstories"))
 
         stories = filter_stories(stories, triggerlist)
 
